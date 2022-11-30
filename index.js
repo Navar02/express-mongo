728289b44022f01e074e3056aeaa6c419d2afe4b
Juan Navarlaz Urtubia <113379066+Navar02@users.noreply.github.com>
2022-11-30 19:29:46 +0100

    actualizacion sin update
    Falta arreglar update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const res = require('express/lib/response');
 const mongojs = require('mongojs')
 const db = mongojs('mongodb://127.0.0.1:27017/test', ['inventory'])
 const app = express();
@@ -10,6 +11,17 @@ app.set('views', './views');
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+app.get("/borrar/:id",(req,res)=>{
+    db.inventory.remove({_id:mongojs.ObjectID(id)},(err,docs)=>{
+        let id=req.params.id
+        if(err){
+            console.log("ERROR AL ELIMINAR ELEMENTO")
+        }else{
+            res.redirect("/inventory")
+        }
+    })
+    res.redirect("/inventory")
+})
 
 app.get('/inventory', (req, res) => {
     db.inventory.find((err, docs) => {
