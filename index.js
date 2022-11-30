3e22d3a7d87e7e29bdbea5b284fd36600f2f86e7
Juan Navarlaz Urtubia <113379066+Navar02@users.noreply.github.com>
2022-11-30 20:24:05 +0100

    Actualizar corecto ya funciona
    funcional

--- index.js
+++ index.js
@@ -52,23 +52,32 @@ app.post("/aniadir",(req,res)=>{
 })})
 //editar
 app.get("/editar/:id",(req,res)=>{
-    db.inventory.find({ _id:mongojs.ObjectId(req.params.id)},(err,docs)=>{
-        if(err){
-            console.log("Error al buscar")
-        }else{
-            res.render("editar")
-            console.log("OK")
+    db.inventory.findOne({_id: mongojs.ObjectId(req.params.id)}, (err, doc) => {
+        if (err) {
+            res.send(err);
+        } else {
+            console.log(doc)
+            res.render('editar', {element: doc})
         }
     })
 })
 
 app.post("/editar/:id",(req,res)=>{
-    console.log("OK")
-    db.inventory.update(
-        {_id:mongojs.ObjectId(req.params.id)},
-        {"item":req.body.item,"qty":req.body.qty,"size":JSON.parse(req.body.size),"status":req.body.status}
-    )
-    res.redirect("/inventory")
+    req.body.size = JSON.parse(req.body.size)
+    console.log(req.body)
+    console.log(req.params.id)
+
+    db.inventory.findAndModify({
+            query: {_id: mongojs.ObjectId(req.params.id)},
+            update: {$set: req.body}
+        },
+        (err, result) => {
+            if (err) {
+                res.send(err)
+            } else {
+                res.redirect('/inventory')
+            }
+        })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
